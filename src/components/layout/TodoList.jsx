5d726db55c595c9da11aefd11e54f9d55e55200d
djskyt <jjsk953563@gmail.com>
2025-09-16 16:49:56 +0900

    ✨ 이것저것
    수정

--- src/components/layout/TodoList.jsx
+++ src/components/layout/TodoList.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
 export default function TodoList() {
-  const [todos, setTodos] = useState(["Todo List"]);
-  const [input, setInput] = useState("");
-
-  const 
+  
 }
