d57249eb91b2b36b4c15755bd619c11470029d2a
djskyt <jjsk953563@gmail.com>
2025-09-16 15:59:13 +0900

    ✨ 프리터
    만듦

--- src/components/layout/Schedule.jsx
+++ src/components/layout/Schedule.jsx
@@ -1,1 +1,15 @@
 import { useState } from "react";
+
+export default function Schedule() {
+  const [form, setForm] = useState({
+    date:"",
+    time:"",
+    title:"",
+    memo:"",
+  });
+  const [list, setList] = useState([]);
+
+  const handleChange = (e) => {
+    const
+  }
+}
