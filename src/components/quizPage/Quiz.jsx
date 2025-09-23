8bc5c6776e6ab9518fc177398df7e475e1ced529
hyeonseo1109 <alsgustj2@naver.com>
2025-09-23 22:34:05 +0900

    ✨ 퀴즈 제목 작성
    퀴즈 제목 작성

--- src/components/quizPage/Quiz.jsx
+++ src/components/quizPage/Quiz.jsx
@@ -3,8 +3,9 @@ import { quizData } from "../../api/dummyQuiz"
 export function Quiz () {
   const data = quizData.data;
   return (<>
-      {data.map((quiz)=>(
-        <h1>{quiz.id}.</h1>
-        ))}
+      {data.map((quiz)=>(<>
+        <h1>{quiz.id}. {quiz.question}</h1>
+        <p></p>
+      </>))}
   </>)
 }
