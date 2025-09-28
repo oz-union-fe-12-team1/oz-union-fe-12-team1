abc31660e18018d1f37e946702e6a20466178307
seun9yoon <seun9yoon@gmail.com>
2025-09-28 22:45:00 +0900

    ✨ 시계 크기 조정
    시계 크기 조정

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -13,7 +13,7 @@ const Ani = ({ children }) => {
 
   return (
     <div
-      className={` w-[5rem] bg-neutral-800 rounded-[4px] p-5 transition-all duration-300 ease-out relative ${
+      className={` w-[6.8rem] bg-neutral-800 rounded-[4px] p-5 transition-all duration-300 ease-out relative ${
         isShown ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-2'
       }`}
     >
@@ -34,10 +34,10 @@ export default function ClockComponent() {
 
   return (
     <div className="flex flex-col justify-center items-center   text-neutral-100 gap-2 font-['HsFallThought20']">
-      <div className="text-4xl">
+      <div className="text-6xl">
         <Ani key={time.hour()}>{String(time.hour()).padStart(2, '0')}</Ani>
       </div>
-      <div className="text-4xl">
+      <div className="text-6xl">
         <Ani key={time.minute()}>
           {String(time.minute()).padStart(2, '0')}
           {/* <div className="text-xs absolute bottom-1 right-1">
