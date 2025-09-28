0f2c328c82de79058e915c01edcaf6e358d61e18
djskyt <jjsk953563@gmail.com>
2025-09-28 22:10:07 +0900

    ✨ 일정버튼
    수정

--- src/components/layout/ScheduleList.jsx
+++ src/components/layout/ScheduleList.jsx
@@ -2,7 +2,7 @@ import ScheduleAdd from "./ScheduleAdd";
 
 export default function ScheduleList({ openAdminDashboard , openAdminPage , openSchedule , list , handleDelete }) {
   return (
-    <div className="rounded-lg bg-white p-4 h-80 w-full overflow-y-auto">
+    <div className="rounded-lg bg-white p-4 h-117 w-full overflow-y-auto">
       {!openAdminDashboard || !openAdminPage ? (
         openSchedule ? (
           <ScheduleAdd list={list} onDelete={handleDelete} />
