e01825446944a6c097417eff3bc5abfffcdaebf7
hyeonseo1109 <alsgustj2@naver.com>
2025-09-30 16:09:59 +0900

    ✨ 수정 확인용 commit
    수정 확인용 commit

--- src/components/adminPage/Admin.jsx
+++ src/components/adminPage/Admin.jsx
@@ -21,7 +21,6 @@ export default function Admin() {
   const searchedUser = debouncedValue ? (userSearchData ?? []) : filteredUsers;
 
   const tableHead = ['이름', '접속여부', '관리자', '이름', '이메일', '가입일시', '계정차단'];
-  //ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ
 
   return (
     <>
